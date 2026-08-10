27708fe001ac4ba2c71056cc25cd334d35f4aa63
Paradrake2 <57878896+Paradrake2@users.noreply.github.com>
2026-08-10 17:43:29 -0400

    Smite icon, changes to smite card

--- Scripts/Cards/Smite.cs
+++ Scripts/Cards/Smite.cs
@@ -7,22 +7,8 @@ public class Smite : Card
     public override void PlayCard(Enemy enemy, Player player, int index, float multiplier = 1f)
     {
         int damage = Mathf.RoundToInt(Damage * multiplier) + player.OutgoingDamageBonus;
-        
-        DamageManager.Instance.ApplyDamageToEnemy(enemy, player, damage / 2, false, index);
-        DamageManager.Instance.ApplyDamageToEnemy(enemy, player, damage / 2, true, index);
-
-        int roll = DiceManager.Instance.RollSecondaryDie();
-        if (roll % 2 == 0)
-        {
-            // even roll, deal magic damage
-            int magicDamage = Mathf.RoundToInt(Int1 * multiplier) + player.OutgoingDamageBonus;
-            DamageManager.Instance.ApplyDamageToEnemy(enemy, player, magicDamage, true, index);
-        }
-        else
-        {
-            // odd roll, deal physical damage
-            int physicalDamage = Mathf.RoundToInt(Int1 * multiplier) + player.OutgoingDamageBonus;
-            DamageManager.Instance.ApplyDamageToEnemy(enemy, player, physicalDamage, false, index);
-        }
+        DamageManager.Instance.ApplyDamageToEnemy(enemy, player, damage, true, index);
+        int secondaryDamage = Mathf.RoundToInt(enemy.GetMaxHealth() * Percentage1) + player.OutgoingDamageBonus;
+        DamageManager.Instance.ApplyDamageToEnemy(enemy, player, secondaryDamage, true, index);
     }
 }
